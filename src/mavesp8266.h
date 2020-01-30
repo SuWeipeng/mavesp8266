0bcb1f5db83d60dff20bdfcd461d53806d6f7372
ubuntu <ubuntu@ubuntu.com>
2020-01-30 23:21:54 +0800

    开启 debug ，防止 GPIO2 因悬空且配置为复位引脚而导致 ESP8266 一直不停复位。

--- src/mavesp8266.h
+++ src/mavesp8266.h
@@ -69,7 +69,7 @@ class MavESP8266GCS;
 #define MAVESP8266_VERSION          ((MAVESP8266_VERSION_MAJOR << 24) & 0xFF00000) | ((MAVESP8266_VERSION_MINOR << 16) & 0x00FF0000) | (MAVESP8266_VERSION_BUILD & 0xFFFF)
 
 //-- Debug sent out to Serial1 (GPIO02), which is TX only (no RX).
-//#define ENABLE_DEBUG
+#define ENABLE_DEBUG
 
 #ifdef ENABLE_DEBUG
 #define DEBUG_LOG(format, ...) do { getWorld()->getLogger()->log(format, ## __VA_ARGS__); } while(0)
